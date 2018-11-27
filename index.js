63439b8be9e56d56f993910c7131ebcc1bf941c2
Will Mildenhall <wtmildenhall@gmail.com>
2018-11-27 13:54:29 -0700

    db stuff

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
-
 const app = express();
+
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({connectionString: connectionString});
+
 const port = process.env.PORT || 5000;
 const dbConnectionString = process.env.DATABASE_URL;
 
 // remember no parentheses here for function call
 app.get("/", testCallback);
+
+app.get("/getData", function(req, res){
+	getData(req, res);
+});
 
 app.listen(port, function(req, res){
 	console.log("Server is listening on port " + port);
@@ -19,3 +29,39 @@ function testCallback(req, res){
 	res.json({name: "will"});
 
 }
+
+function getData(req, res) {
+	var id = req.query.id;
+
+	getTitleFromDb(id, function(error, result){
+		if (error || result == null || result.length != 1) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			var person = result[0];
+			response.status(200).json(result[0]);
+		}
+	})
+}
+
+function getTitleFromDb(id, callback) {
+	console.log("Getting person from DB with id: " + id);
+
+	var sql = "SELECT id, title FROM person WHERE id = $1::int";
+
+	var params = [id];
+
+	pool.query(sql, params, function(err, result) {
+
+		if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(result.rows));
+
+		callback(null, result.rows);
+	});
+
+} 
