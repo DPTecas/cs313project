580223f801349c31a24784172c1abd35a12a1cd6
Will Mildenhall <wtmildenhall@gmail.com>
2018-12-06 13:13:43 -0700

    json stuff

--- ajaxStuff.js
+++ ajaxStuff.js
@@ -1,10 +1,7 @@
-function addPath() {
-
-    var xhttp = new XMLHttpRequest();
-  	xhttp.onreadystatechange = function() {
-    	if (this.readyState == 4 && this.status == 200) {
-     		document.getElementById("path1").innerHTML = "hi im new";
-   		}
-	}
-	
+function addPath()
+	{$(document).ready(function(){
+	    $("#path1").click(function(){
+	        $(this).hide();
+	    });
+	});
 }
