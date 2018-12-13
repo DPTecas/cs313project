69eec42072f56bf3cdd6dac796122f79904ff07f
Will Mildenhall <wtmildenhall@gmail.com>
2018-12-13 16:28:41 -0700

    please

--- ajaxStuff.js
+++ ajaxStuff.js
@@ -12,11 +12,11 @@ $(function()
 		type: "GET",
         url: 'https://glacial-sands-13729.herokuapp.com/getScores?id=' + value,
         success: function (result) {
-        	for (var i = 9; i >= 0; i--)
-        	{
-        		if (result[i] != undefined)
-        			$('#b'+i).html(result[i].name + result[i].score + result[i].category);
-        	}
+        
+        	
+        if (result[0] != undefined)
+        	$('#b1').html(result[0].name + result[0].score + result[0].category);
+        	
         	$("#board").dialog("open");
         }
     });
