3062bd4be5853dde711a2312f48b3fdf8c6e7d60
Will Mildenhall <wtmildenhall@gmail.com>
2018-12-13 14:46:14 -0700

    new feature

--- ajaxStuff.js
+++ ajaxStuff.js
@@ -12,7 +12,7 @@ $(function()
 		type: "GET",
         url: 'https://glacial-sands-13729.herokuapp.com/getScores?id=' + value,
         success: function (result) {
-        	for (var i = 10; i > 0; i--)
+        	for (var i = 9; i >= 0; i--)
         	{
         		tag = '#b' + i;
         		if (result[i] != undefined)
