bf5119d1ad945935881deccdd2304bdb5bf29e93
Will Mildenhall <wtmildenhall@gmail.com>
2018-12-17 14:16:09 -0800

    score stuff

--- database.sql
+++ database.sql
@@ -59,33 +59,33 @@ VALUES (2,
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('You see your ex at the end of the aisle!',
-		'Hide behind the largest sack of flour you see.', 
-		'Run up and give them a big hug.', 
-		'Tell them you want them back, you would give anything...');
+		'1) Tell them you want them back, you would give anything...', 
+		'2) Run up and give them a big hug.', 
+		'3) Hide behind the largest sack of flour you see.');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (3, 
-		'We both know you aren''t small enough to fit behond that sack.',
+		'You deserve better. Probably. I don''t know.',
 		'Could you be more awkward?',
-		'You deserve better. Probably. I don''t know.');
+		'We both know you aren''t small enough to fit behind that sack.');
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('The chips n'' dip have called your name.',
-		'Grab chips n'' dip, why else would you be here?', 
-		'Don''t grab chips n'' dip', 
-		'Buy two chips n'' two dips');
+		'1) Don''t grab chips n'' dip.', 
+		'2) Buy two chips n'' two dips.', 
+		'3) Grab chips n'' dip, why else would you be here?');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (4, 
-		'Acceptable.',
-		'Are you kidding me? Get out. You aren''t welcome here anymore',
-		'You are my hero.');
+		'Are you kidding me? Get out. You aren''t welcome here anymore.',
+		'You are my hero.',
+		'Acceptable.');
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('You run into Deborah, an old friend, and her child, Jimothy.',
-		'Hey Diane! Hey Johnny! How are ya????', 
-		'How you doin?', 
-		'Ignore her. Run. RUN.');
+		'1) Hey Diane! Hey Johnny! How are ya????', 
+		'2) How you doin?', 
+		'3)  her. Run. RUN.');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (5, 
@@ -95,9 +95,9 @@ VALUES (5,
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('You make your way to the produce aisle...',
-		'Grab a steak.', 
-		'Grab a giant bag of frozen chicken.', 
-		'Grab some tofu.');
+		'1) Grab a steak.', 
+		'2) Grab a giant bag of frozen chicken.', 
+		'3) Grab some tofu.');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (6, 
@@ -107,33 +107,33 @@ VALUES (6,
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('You run into a small child! Where is her mother??',
-		'Help her find her mother.', 
-		'Challenge her to a foot race.', 
-		'Wrong aisle...');
+		'1) Challenge her to a foot race.', 
+		'2) Help her find her mother.', 
+		'3) Wrong aisle...');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (7, 
-		'You seem to have a heart. I''m so proud.',
 		'You win. The child tripped, and now has a bloody nose. You blew it.',
+		'You seem to have a heart. I''m so proud.',
 		'Have you no heart?');
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('You''ve entered the most glorious hall in the grocery kindom',
-		'Dr Pepper? Check. Mountain Dew? Check.', 
-		'Get a 24 pack of premium water', 
-		'Buy enough soda to bathe in.');
+		'1) Buy enough soda to bathe in.', 
+		'2) Dr Pepper? Check. Mountain Dew? Check.', 
+		'3) Get a 24 pack of premium water.');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (8, 
+		'Weird flex, but I respect it.',
 		'The nectar of life, and the elixir of life, respectively. Smart choices.',
-		'I mean.. good for you. Great, in fact. Yeah.',
-		'Weird flex, but I respect it');
+		'I mean.. good for you. Great, in fact. Yeah.');
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('Lobsters, crabs, and shrimp oh my!',
-		'Reach into the lobster tank, grab a lobster, and release your battle cry.', 
-		'Get crab legs and shrimp.. and more crab legs.', 
-		'You''re poor. Just walk away now.');
+		'1) Reach into the lobster tank, grab a lobster, and release your battle cry.', 
+		'2) Get crab legs and shrimp.. and more crab legs.', 
+		'3) You''re poor. Just walk away now.');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (9, 
@@ -143,12 +143,12 @@ VALUES (9,
 
 INSERT INTO dialogues (prompt, option1, option2, option3) 
 VALUES ('Time to check out! Wait.. you seem to have left your wallet at home.',
-		'Run out the door screaming "FREEDOOMMMM!"', 
-		'Look at the cashier. Cry.', 
-		'Beg the old man behind you for a loan.');
+		'1) Run out the door screaming "FREEDOOMMMM!"', 
+		'2) Beg the old man behind you for a loan.', 
+		'3) Look at the cashier. Cry.');
 
 INSERT INTO responses (dia_id, r1, r2, r3)
 VALUES (10, 
 		'FREEEDOMMMMMM!!!',
-		'There, there. We all fail. Except me. I don''t fail',
-		'He gives you the loan! Inspirational.');
+		'He gives you the loan! Inspirational.',
+		'There, there. We all fail. Except me. I don''t fail');
